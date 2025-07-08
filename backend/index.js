a9a5c7a46d906e774fafcae1ddca57c42fc614f4
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2025-07-08 19:11:08 +0530

    try-to-fix-cors issue

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ import passwordRoutes from "./routes/passwordRoutes.js";
 import resumeRoutes from "./routes/resumeRoutes.js";
 
 const app = express();
-app.use(cors("*"));
+app.use(cors({
+  origin: "https://resume-nybo1uhdp-surbhi-singhals-projects.vercel.app/", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api', authRoutes);
 // app.use("/api/user", userRoutes);
